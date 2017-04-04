ecf84fa320fbd78329077d7ba542e9849c9f2433
Sébastien Demanou <demsking@gmail.com>
2017-04-04 19:26:48 +0200

    Add Standard Style

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
-var request = require('request');
-var path = require('path');
-var fs = require('fs');
+'use strict'
 
-function on_error(err, options) {
-    if (options.done) {
-        return options.done(err);
-    }
-    
-    throw err;
+const request = require('request')
+const path = require('path')
+const fs = require('fs')
+
+const onError = (err, options) => {
+  if (options.done) {
+    return options.done(err)
+  }
+  throw err
 }
 
-module.exports = function(options) {
-    if (!options.url) {
-        throw new Error('The option url is required');
-    }
-    
-    if (!options.dest) {
-        throw new Error('The option dest is required');
-    }
-    
-    request({url: options.url, encoding: null}, function (err, res, body) {
-        if (err) { 
-            return on_error(err, options);
-        }
+module.exports = (options) => {
+  if (!options.url) {
+    throw new Error('The option url is required')
+  }
 
-        if (body && res.statusCode === 200) {
-            if (!fs.existsSync(options.dest)){
-                fs.mkdirSync(options.dest);
-                console.log("Destination folder didn't existed. Folder created.");
-            }
-            
-            if (!path.extname(options.dest)) {
-                options.dest = path.join(options.dest, path.basename(options.url));
-            }
-            
-            fs.writeFile(options.dest, body, 'binary', function(err){
-                if (err) {
-                    return on_error(err, options);
-                }
-                options.done && options.done(false, options.dest, body)
-            });
-        } else {
-            if (!body) { 
-                return on_error(new Error('Image loading error - empty body. URL: ' + options.url), options); 
-            } else { 
-                return on_error(new Error('Image loading error - ' + res.statusCode + '. URL: ' + options.url), options); 
-            }
+  if (!options.dest) {
+    throw new Error('The option dest is required')
+  }
+
+  request({url: options.url, encoding: null}, (err, res, body) => {
+    if (err) {
+      return onError(err, options)
+    }
+
+    if (body && res.statusCode === 200) {
+      if (!fs.existsSync(options.dest)) {
+        fs.mkdirSync(options.dest)
+        console.log("Destination folder didn't existed. Folder created.")
+      }
+
+      if (!path.extname(options.dest)) {
+        options.dest = path.join(options.dest, path.basename(options.url))
+      }
+
+      fs.writeFile(options.dest, body, 'binary', (err) => {
+        if (err) {
+          return onError(err, options)
         }
-    });
-};
+        options.done && options.done(false, options.dest, body)
+      })
+    } else {
+      if (!body) {
+        return onError(new Error('Image loading error - empty body. URL: ' + options.url), options)
+      }
+      onError(new Error('Image loading error - ' + res.statusCode + '. URL: ' + options.url), options)
+    }
+  })
+}
